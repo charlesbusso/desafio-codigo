159b1ee323e6e2e94a039eae2cd97967b52835d3
charles busso <109078429+charlesbusso@users.noreply.github.com>
2023-02-23 09:20:20 -0300

    ok

---
+++ script.js
@@ -0,0 +1,28 @@
+let msg1 = document.getElementById('num1');
+let num2 = document.getElementById('num2');
+let num3 = document.getElementById('num3');
+let num4 = document.getElementById('num4');
+let nome =document.getElementById('nome');
+let mes = document.getElementById('mes');
+
+let numA = document.getElementById('numA');
+let numB = document.getElementById('numB');
+let numC = document.getElementById('numC');
+let numD = document.getElementById('numD');
+let form = document.getElementById('form');
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+     cartao();
+
+});
+function cartao(){
+    
+    if(numA === ""){
+        msg1.innerHTML = 'AAAA';
+    }else {(numA)}
+}
+window.addEventListener("load");
+
+
