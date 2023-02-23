5cfa504abd932742fcfafcc90849d6ef696ee1f3
charles busso <charlesagbusso13@gmail.com>
2023-02-23 19:04:01 -0300

    NumA

--- script.js
+++ script.js
@@ -4,8 +4,6 @@ let num3 = document.getElementById('num3');
 let num4 = document.getElementById('num4');
 let nome =document.getElementById('nome');
 let mes = document.getElementById('mes');
-
-let numA = document.getElementById('numA');
 let numB = document.getElementById('numB');
 let numC = document.getElementById('numC');
 let numD = document.getElementById('numD');
