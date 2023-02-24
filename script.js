2ff99b48f2dc4e698516719133c9f6daba594177
charles busso <109078429+charlesbusso@users.noreply.github.com>
2023-02-24 08:19:02 -0300

    ok

--- script.js
+++ script.js
@@ -5,16 +5,22 @@ function capturar(){
   let capturandoB = "";
   let capturandoC = "";
   let capturandoD = "";
+  let capturandoE = "";
+  let capturandoF = "";
 
 
     capturandoA = document.getElementById('numA').value; 
     capturandoB = document.getElementById('numB').value;
     capturandoC = document.getElementById('numC').value;
     capturandoD = document.getElementById('numD').value;
+    capturandoE = document.getElementById('ano').value;
+    capturandoF = document.getElementById('name').value;
     document.getElementById('num1').innerHTML = capturandoA;
     document.getElementById('num2').innerHTML = capturandoB;
     document.getElementById('num3').innerHTML =capturandoC;
     document.getElementById('num4').innerHTML = capturandoD;
+    document.getElementById('mes').innerHTML = capturandoE;
+    document.getElementById('nome').innerHTML = capturandoF;
 
 }
 window.addEventListener("load", capturar);
